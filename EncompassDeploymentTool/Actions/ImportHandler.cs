96c5e984778c64322b67a6096983af9d9c0f6ea2
Daniel Laughland <dlaughland@novushomemortgage.com>
2019-04-16 17:55:19 -0500

    Does the package import happy path

--- EncompassDeploymentTool/Actions/ImportHandler.cs
+++ EncompassDeploymentTool/Actions/ImportHandler.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using EllieMae.EMLite.Client;
+using EllieMae.EMLite.Packages;
+using System;
 
 namespace EncompassDeploymentTool.Actions
 {
     public class ImportHandler
     {
+        private readonly IConnection conn;
+
+        public ImportHandler() : this(EllieMae.EMLite.RemotingServices.Session.Connection) { }
+
+        public ImportHandler(IConnection conn)
+        {
+            this.conn = conn;
+        }
+
         public int Execute(ImportPackageOptions options)
         {
             Console.WriteLine($"Importing Package {options.PackagePath}");
-            return 0;
+
+            var importer = new PackageImporter(conn, PackageImportConflictOption.Overwrite);
+            var package = new ExportPackage(options.PackagePath);
+
+            var success = importer.Import(package);
+
+            return success ? 0 : 1;
         }
     }
 }
