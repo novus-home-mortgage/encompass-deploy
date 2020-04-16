d1bbe68e18f7abc03c3838fcc1e387df51fe0915
Daniel Laughland <dlaughland@panoramamortgage.com>
2020-04-16 14:23:02 -0500

    An idea for handling differently-versioned environments
    Bend the registry to our will

--- EncompassDeploymentTool/Program.cs
+++ EncompassDeploymentTool/Program.cs
@@ -1,5 +1,9 @@
-﻿using CommandLine;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CommandLine;
 using EncompassDeploymentTool.Actions;
+using Microsoft.Win32;
 
 namespace EncompassDeploymentTool
 {
@@ -7,10 +11,8 @@ namespace EncompassDeploymentTool
     {
         static int Main(string[] args)
         {
-            new EllieMae.Encompass.Runtime.RuntimeServices().Initialize();
-
             return Parser.Default.ParseArguments<GetFormOptions, PackOptions, ImportPackageOptions, UpdateCDOOptions, LinkFormOptions>(args)
-                .WithParsed<EncompassConnectionOptions>(StartEncompassSession)
+                .WithParsed<EncompassConnectionOptions>(ConfigureSmartClient)
                 .MapResult(
                     (GetFormOptions opts) => new GetFormHandler().Execute(opts),
                     (PackOptions opts) => new PackHandler().Execute(opts),
@@ -21,6 +23,31 @@ namespace EncompassDeploymentTool
                 );
         }
 
+        static void ConfigureSmartClient(EncompassConnectionOptions options)
+        {
+            // This tool is meant to be run in automated environments.
+            // That means no picking your instance with the AppLauncher.
+            // If you regularly connect to multiple environments and they are running different
+            // Encompass versions, you're likely to encounter VersionMismatchExceptions.
+
+            // To remedy, let's manipulate the registry in the same way that AppLauncher does
+            var path = Assembly.GetExecutingAssembly().Location;
+            var dir = Path.GetDirectoryName(path).Replace('\\', '/');
+            var keyName = $@"SOFTWARE\Ellie Mae\SmartClient\{dir}";
+
+            var key = Registry.LocalMachine.OpenSubKey(keyName, true);
+            key.SetValue("AutoSignOn", "1", RegistryValueKind.String);
+            key.SetValue("Credentials", GetCredentials(options.InstanceName), RegistryValueKind.String);
+            key.SetValue("SmartClientIDs", options.InstanceName, RegistryValueKind.String);
+            key.Close();
+
+            // Now we can fire up the Ellie stuff
+            new EllieMae.Encompass.Runtime.RuntimeServices().Initialize();
+            StartEncompassSession(options);
+        }
+
+        // Must start the session in a different method because .NET can't bind ClientSession.dll
+        // into the execution context *at all* until the Encompass runtime is initialized
         static void StartEncompassSession(EncompassConnectionOptions options)
         {
             EllieMae.EMLite.RemotingServices.Session.Start(
@@ -29,6 +56,26 @@ namespace EncompassDeploymentTool
                 options.Password,
                 "encompass-deploy"
             );
+        }
+
+        // When you have a static encryption key, and you put it right next to your
+        // encryption code, you don't have encryption code anymore.
+        static string GetCredentials(string instanceId)
+        {
+            var credentials = $"{instanceId}\n{instanceId}";
+
+            var encryptorType = Type.GetType("EllieMae.Encompass.AsmResolver.Utils.XT, EllieMae.Encompass.AsmResolver, Version=1.0.0.0, Culture=neutral, PublicKeyToken=d11ef57bba4acf91");
+            var resolverConstsType = typeof(EllieMae.Encompass.AsmResolver.Utils.ResolverConsts);
+
+            var keyProperty = resolverConstsType.GetField("KB64", BindingFlags.Static | BindingFlags.NonPublic);
+            var key = keyProperty.GetValue(null) as string;
+
+            var encyptorMethod = encryptorType.GetMethod("ESB64", BindingFlags.Static | BindingFlags.NonPublic);
+
+            return encyptorMethod.Invoke(null, new[]
+            {
+                credentials, key
+            }) as string;
         }
     }
 }
